37df88c19a692aeaec38999d26e78e5189ab3b1e
SCT\priya.dalvi <priya.dalvi@springct.com>
2025-08-12 15:52:16 +0530

    added component

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 // const heading = React.createElement(
 //             "h1",
-//             { id: "title", key: "h1" },
+//             { id: "title", key: "h1" }, 
 //             "Hello Namaste from React"
 //         );
 
@@ -11,17 +11,34 @@ import ReactDOM from "react-dom/client";
 //         root.render(heading);
 
 
-const parent = React.createElement(
-            "div", {id: "parent", key: "div1"},
-            [
-                React.createElement("div", {id: "child1", key: "div2"}, [
-                    React.createElement("h1", {key: "h2"}, "I am an h1 tag"),
-                    React.createElement("h2", {key: "h3"}, "I am an h2 tag")
-                ]),
-                React.createElement("div", {id: "child2", key: "div3"}, [
-                    React.createElement("h1", {key: "h4"}, "I am an h1 tag"),
-                    React.createElement("h2", {key: "h5"}, "I am an h2 tag")
-                ])
-            ]);
-        const parentInRoot = ReactDOM.createRoot(document.getElementById("root"));
-        parentInRoot.render(parent);
+// const parent = React.createElement(
+//             "div", {id: "parent", key: "div1"},
+//             [
+//                 React.createElement("div", {id: "child1", key: "div2"}, [
+//                     React.createElement("h1", {key: "h2"}, "I am an h1 tag"),
+//                     React.createElement("h2", {key: "h3"}, "I am an h2 tag")
+//                 ]),
+//                 React.createElement("div", {id: "child2", key: "div3"}, [
+//                     React.createElement("h1", {key: "h4"}, "I am an h1 tag"),
+//                     React.createElement("h2", {key: "h5"}, "I am an h2 tag")
+//                 ])
+//             ]);
+
+
+const title = (
+    <h1 id="title" key="h1">Hello React using JSX</h1>
+);
+
+const number = 10;
+
+const MainComponent = () => {
+    return (
+        <div id="parent" key="div1">
+            <h1> Writing number using javascript inside JSX component </h1>
+            {number}
+        </div>
+    );
+};
+
+const parentInRoot = ReactDOM.createRoot(document.getElementById("root"));
+parentInRoot.render(<MainComponent/>);
